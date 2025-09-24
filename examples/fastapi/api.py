ad8ed80ce63e1701acf4ea9a6994e0d7bcd9dfe2
AlxcNL <J.A.Boogaard@hr.nl>
2025-09-24 08:45:12 +0200

    Minor change

--- examples/fastapi/api.py
+++ examples/fastapi/api.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 
 from fastapi import FastAPI
+from fastapi import FastAPI
+
 
 import uvicorn
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight": {"theme": "obsidian"}})
+
 port = 8000
 
 # GET
 @app.get("/")
 async def helloWorld():
     return { "message" : "hello world" }
-
-# # GET path parameter
-# @app.get("/component/{component_id}")
-# async def getComponent(component_id: int):
-#     return { "component_id" : component_id }
 
 @app.get("/component")
 async def readComponent(text: str):    
